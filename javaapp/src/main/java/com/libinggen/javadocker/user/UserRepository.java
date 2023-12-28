1974afe33d3216ea7964d252d4926cb4c0e76842
libinggen <libinggengit@gmail.com>
2023-12-28 22:38:06 +0900

    ok

--- javaapp/src/main/java/com/libinggen/javadocker/user/UserRepository.java
+++ javaapp/src/main/java/com/libinggen/javadocker/user/UserRepository.java
@@ -1,5 +1,10 @@
 package com.libinggen.javadocker.user;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
+
