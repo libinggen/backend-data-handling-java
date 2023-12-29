85add2c14e44e5f86c5ea1b705d3749935d3540f
libinggen <libinggengit@gmail.com>
2023-12-30 08:19:23 +0900

    add uuid

--- javaapp/src/main/java/com/libinggen/javadocker/javaapp/user/User.java
+++ javaapp/src/main/java/com/libinggen/javadocker/javaapp/user/User.java
@@ -1,77 +1,77 @@
 package com.libinggen.javadocker.javaapp.user;
 
+import java.util.UUID;
+import org.hibernate.annotations.GenericGenerator;
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-  @Column(name = "uuid")
-  private String uuid;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "uuid", updatable = false, nullable = false)
+    private UUID uuid;
 
-  @Column(name = "userName")
-  private String userName;
+    @Column(name = "userName")
+    private String userName;
 
-  @Column(name = "email")
-  private String email;
+    @Column(name = "email")
+    private String email;
 
-  @Column(name = "password")
-  private String password;
+    @Column(name = "password")
+    private String password;
 
-  @Column(name = "password2")
-  private String password2;
+    @Column(name = "password2")
+    private String password2;
 
-  //getters and setters
+    // getters and setters
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getUuid() {
-    return uuid;
-  }
+    public UUID getUuid() {
+        return uuid;
+    }
 
-  public void setUuid(String uuid) {
-    this.uuid = uuid;
-  }
+    public String getUserName() {
+        return userName;
+    }
 
-  public String getUserName() {
-    return userName;
-  }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public String getPassword2() {
+        return password2;
+    }
 
-  public String getPassword2() {
-    return password2;
-  }
-
-  public void setPassword2(String password2) {
-    this.password2 = password2;
-  }
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
 }
