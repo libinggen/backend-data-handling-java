219f5484676ceeaf53a75660517576dae06332af
libinggen <libinggengit@gmail.com>
2024-01-07 08:54:32 +0900

    ok

--- javaapp/src/main/java/com/libinggen/javadocker/javaapp/user/User.java
+++ javaapp/src/main/java/com/libinggen/javadocker/javaapp/user/User.java
@@ -5,7 +5,12 @@ import org.hibernate.annotations.GenericGenerator;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import org.hibernate.validator.constraints.*;
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
@@ -38,51 +43,5 @@ public class User {
         if (uuid == null) {
             uuid = UUID.randomUUID();
         }
-    }
-
-    // getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 }
